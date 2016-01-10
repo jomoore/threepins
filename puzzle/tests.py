1fae7768f33e20ec801a8806018fffa2717e0c05
Jeremy Moore <jmoore@ieee.org>
2016-01-10 11:39:28 +0200

    Add tests for create_grid.

--- puzzle/tests.py
+++ puzzle/tests.py
@@ -1,8 +1,9 @@
 from datetime import timedelta, datetime
 from django.test import TestCase
 from django.utils import timezone
-from puzzle.models import Author, Puzzle
+from puzzle.models import Author, Puzzle, Entry
 from puzzle.feeds import PuzzleFeed
+from puzzle.views import create_grid
 
 class PuzzleModelTests(TestCase):
     def test_default_author(self):
@@ -72,3 +73,54 @@ class FeedTests(TestCase):
             self.create_puzzle(i, timezone.now() - timedelta(days=num_puzzles - i))
         feed = PuzzleFeed()
         self.assertEqual(feed.items().count(), limit)
+
+class GridCreationTests(TestCase):
+    def create_small_puzzle(self):
+        size = 3
+        a = Author.objects.create(name='Test Author')
+        p = Puzzle.objects.create(size=size)
+        e0 = Entry.objects.create(puzzle=p, clue='e0', answer='ab c', x=0, y=0, down=False)
+        e1 = Entry.objects.create(puzzle=p, clue='e1', answer='x-yz', x=0, y=2, down=False)
+        e2 = Entry.objects.create(puzzle=p, clue='e2', answer='amx', x=0, y=0, down=True)
+        e3 = Entry.objects.create(puzzle=p, clue='e3', answer='cnz', x=2, y=0, down=True)
+        return p
+
+    def test_create_grid_pattern(self):
+        grid = create_grid(self.create_small_puzzle(), 3)
+        for row in range(3):
+            for col in range(3):
+                self.assertEqual(grid[row][col]['row'], row)
+                self.assertEqual(grid[row][col]['col'], col)
+                if row == 1 and col == 1:
+                    self.assertFalse('light' in grid[row][col]['type'])
+                    self.assertTrue('block' in grid[row][col]['type'])
+                else:
+                    self.assertTrue('light' in grid[row][col]['type'])
+                    self.assertFalse('block' in grid[row][col]['type'])
+
+    def test_create_grid_clue_numbers(self):
+        grid = create_grid(self.create_small_puzzle(), 3)
+        expected_numbers = [[1, None, 2], [None, None, None], [3, None, None]]
+        for row in range(3):
+            for col in range(3):
+                self.assertEqual(grid[row][col]['number'], expected_numbers[row][col])
+
+    def test_create_grid_letters(self):
+        grid = create_grid(self.create_small_puzzle(), 3)
+        expected_letters = [['A', 'B', 'C'], ['M', None, 'N'], ['X', 'Y', 'Z']]
+        for row in range(3):
+            for col in range(3):
+                self.assertEqual(grid[row][col]['letter'], expected_letters[row][col])
+
+    def test_create_grid_borders(self):
+        grid = create_grid(self.create_small_puzzle(), 3)
+        for row in range(3):
+            for col in range(3):
+                if row == 0:
+                    self.assertTrue('topmost' in grid[row][col]['type'])
+                else:
+                    self.assertFalse('topmost' in grid[row][col]['type'])
+                if col == 0:
+                    self.assertTrue('leftmost' in grid[row][col]['type'])
+                else:
+                    self.assertFalse('leftmost' in grid[row][col]['type'])
