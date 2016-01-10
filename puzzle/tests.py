f9a03077bb8f1c8c86b6331105c53c15e29b75ea
Jeremy Moore <jmoore@ieee.org>
2016-01-10 19:25:53 +0200

    Added tests for puzzle views.

--- puzzle/tests.py
+++ puzzle/tests.py
@@ -1,9 +1,26 @@
 from datetime import timedelta, datetime
 from django.test import TestCase
 from django.utils import timezone
+from django.core.urlresolvers import reverse
 from puzzle.models import Author, Puzzle, Entry
 from puzzle.feeds import PuzzleFeed
 from puzzle.views import create_grid, get_clues, get_date_string
+
+def create_small_puzzle():
+    size = 3
+    a = Author.objects.create(name='Test Author')
+    p = Puzzle.objects.create(size=size)
+    e0 = Entry.objects.create(puzzle=p, clue='1a', answer='ab c', x=0, y=0, down=False)
+    e1 = Entry.objects.create(puzzle=p, clue='3a', answer='x-yz', x=0, y=2, down=False)
+    e2 = Entry.objects.create(puzzle=p, clue='1d', answer='amx', x=0, y=0, down=True)
+    e3 = Entry.objects.create(puzzle=p, clue='2d', answer='cnz', x=2, y=0, down=True)
+    return p
+
+def create_puzzle_range():
+    for i in range(-2, 3):
+        p = create_small_puzzle()
+        p.pub_date = pub_date=timezone.now() + timedelta(days=i)
+        p.save()
 
 class PuzzleModelTests(TestCase):
     def test_default_author(self):
@@ -29,14 +46,14 @@ class PuzzleModelTests(TestCase):
     def test_published_puzzle_url(self):
         a = Author.objects.create(name='Test Author')
         p = Puzzle.objects.create(pub_date=timezone.now())
-        self.assertFalse('preview' in p.get_absolute_url())
-        self.assertTrue(str(p.number) in p.get_absolute_url())
+        self.assertNotIn('preview', p.get_absolute_url())
+        self.assertIn(str(p.number), p.get_absolute_url())
 
     def test_preview_puzzle_url(self):
         a = Author.objects.create(name='Test Author')
         p = Puzzle.objects.create(pub_date=timezone.now() + timedelta(days=1))
-        self.assertTrue('preview' in p.get_absolute_url())
-        self.assertTrue(str(p.number) in p.get_absolute_url())
+        self.assertIn('preview', p.get_absolute_url())
+        self.assertIn(str(p.number), p.get_absolute_url())
 
 class FeedTests(TestCase):
     def get_author(self):
@@ -74,16 +91,6 @@ class FeedTests(TestCase):
         feed = PuzzleFeed()
         self.assertEqual(feed.items().count(), limit)
 
-def create_small_puzzle():
-    size = 3
-    a = Author.objects.create(name='Test Author')
-    p = Puzzle.objects.create(size=size)
-    e0 = Entry.objects.create(puzzle=p, clue='1a', answer='ab c', x=0, y=0, down=False)
-    e1 = Entry.objects.create(puzzle=p, clue='3a', answer='x-yz', x=0, y=2, down=False)
-    e2 = Entry.objects.create(puzzle=p, clue='1d', answer='amx', x=0, y=0, down=True)
-    e3 = Entry.objects.create(puzzle=p, clue='2d', answer='cnz', x=2, y=0, down=True)
-    return p
-
 class GridCreationTests(TestCase):
     def test_create_grid_pattern(self):
         grid = create_grid(create_small_puzzle(), 3)
@@ -92,11 +99,11 @@ class GridCreationTests(TestCase):
                 self.assertEqual(grid[row][col]['row'], row)
                 self.assertEqual(grid[row][col]['col'], col)
                 if row == 1 and col == 1:
-                    self.assertFalse('light' in grid[row][col]['type'])
-                    self.assertTrue('block' in grid[row][col]['type'])
+                    self.assertNotIn('light', grid[row][col]['type'])
+                    self.assertIn('block', grid[row][col]['type'])
                 else:
-                    self.assertTrue('light' in grid[row][col]['type'])
-                    self.assertFalse('block' in grid[row][col]['type'])
+                    self.assertIn('light', grid[row][col]['type'])
+                    self.assertNotIn('block', grid[row][col]['type'])
 
     def test_create_grid_clue_numbers(self):
         grid = create_grid(create_small_puzzle(), 3)
@@ -117,13 +124,13 @@ class GridCreationTests(TestCase):
         for row in range(3):
             for col in range(3):
                 if row == 0:
-                    self.assertTrue('topmost' in grid[row][col]['type'])
+                    self.assertIn('topmost', grid[row][col]['type'])
                 else:
-                    self.assertFalse('topmost' in grid[row][col]['type'])
+                    self.assertNotIn('topmost', grid[row][col]['type'])
                 if col == 0:
-                    self.assertTrue('leftmost' in grid[row][col]['type'])
+                    self.assertIn('leftmost', grid[row][col]['type'])
                 else:
-                    self.assertFalse('leftmost' in grid[row][col]['type'])
+                    self.assertNotIn('leftmost', grid[row][col]['type'])
 
 class ClueCreationTests(TestCase):
     def test_get_clues(self):
@@ -154,3 +161,131 @@ class DateFormattingTests(TestCase):
         a = Author.objects.create(name='Test Author')
         p = Puzzle.objects.create(pub_date=dt)
         self.assertEquals(get_date_string(p), '04 Mar 1980')
+
+class PuzzleViewTests(TestCase):
+    def test_home_page_grid_squares(self):
+        create_puzzle_range()
+        response = self.client.get('/')
+        self.assertContains(response, 'class="puzzle"')
+        self.assertContains(response, 'id="grid"')
+        self.assertEqual(response.content.count('class="light'), 8)
+        self.assertEqual(response.content.count('class="block'), (15 * 15) - 8)
+        self.assertEqual(response.content.count('topmost'), 15)
+        self.assertEqual(response.content.count('leftmost'), 15)
+        self.assertEqual(response.content.count('class="grid-number"'), 3)
+        self.assertEqual(response.content.count('data-a='), 8)
+        for i in range(3):
+            self.assertEqual(response.content.count('data-x="%i"' % i), 15)
+            self.assertEqual(response.content.count('data-y="%i"' % i), 15)
+
+    def test_home_page_clues(self):
+        create_puzzle_range()
+        response = self.client.get('/')
+        self.assertContains(response, 'clue-box')
+        self.assertEqual(response.content.count('clue-number'), 4)
+
+    def test_home_page_wrapping(self):
+        create_puzzle_range()
+        response = self.client.get('/')
+        self.assertContains(response, '<title>Three Pins - A cryptic crossword outlet</title>')
+        self.assertContains(response, '<meta name="description" content="A free interactive site')
+        self.assertContains(response, '&lt; Previous')
+        self.assertNotContains(response, 'Next &gt;')
+        self.assertContains(response, reverse('solution', args=[2]))
+
+    def test_home_page_contains_latest(self):
+        create_puzzle_range()
+        response = self.client.get('/')
+        self.assertNotContains(response, 'data-number="1"')
+        self.assertContains(response, 'data-number="2"')
+        self.assertNotContains(response, 'data-number="3"')
+
+    def test_home_page_answers_hidden(self):
+        create_puzzle_range()
+        response = self.client.get('/')
+        self.assertNotContains(response, 'class="letter"')
+
+    def test_puzzle_without_previous(self):
+        create_puzzle_range()
+        response = self.client.get(reverse('puzzle', args=[0]))
+        self.assertContains(response, 'class="puzzle"')
+        self.assertContains(response, 'id="grid"')
+        self.assertContains(response, 'data-number="0"')
+        self.assertNotContains(response, '&lt; Previous')
+        self.assertContains(response, 'Next &gt;')
+        self.assertContains(response, '<title>Three Pins - Crossword #0</title>')
+
+    def test_puzzle_with_next_and_previous(self):
+        create_puzzle_range()
+        response = self.client.get(reverse('puzzle', args=[1]))
+        self.assertContains(response, 'class="puzzle"')
+        self.assertContains(response, 'id="grid"')
+        self.assertContains(response, 'data-number="1"')
+        self.assertContains(response, '&lt; Previous')
+        self.assertContains(response, 'Next &gt;')
+        self.assertContains(response, '<title>Three Pins - Crossword #1</title>')
+
+    def test_future_puzzle_not_accessible(self):
+        create_puzzle_range()
+        response = self.client.get(reverse('puzzle', args=[3]))
+        self.assertEqual(response.status_code, 404)
+
+    def test_preview_future_puzzle(self):
+        create_puzzle_range()
+        response = self.client.get(reverse('preview', args=[3]))
+        self.assertContains(response, 'class="puzzle"')
+        self.assertContains(response, 'id="grid"')
+        self.assertContains(response, 'data-number="3"')
+        self.assertNotContains(response, 'class="letter"')
+        self.assertContains(response, '<title>Three Pins - Preview #3</title>')
+
+    def test_solution_available(self):
+        create_puzzle_range()
+        response = self.client.get(reverse('solution', args=[2]))
+        self.assertContains(response, 'class="puzzle"')
+        self.assertContains(response, 'id="grid"')
+        self.assertContains(response, 'data-number="2"')
+        self.assertEqual(response.content.count('class="letter"'), 8)
+        self.assertContains(response, '<title>Three Pins - Solution #2</title>')
+
+    def test_preview_solution_available(self):
+        create_puzzle_range()
+        response = self.client.get(reverse('preview_solution', args=[3]))
+        self.assertContains(response, 'class="puzzle"')
+        self.assertContains(response, 'id="grid"')
+        self.assertContains(response, 'data-number="3"')
+        self.assertEqual(response.content.count('class="letter"'), 8)
+        self.assertContains(response, '<title>Three Pins - Solution #3</title>')
+
+    def test_invalid_puzzle(self):
+        create_puzzle_range()
+        response = self.client.get(reverse('puzzle', args=[100]))
+        self.assertEqual(response.status_code, 404)
+
+    def test_invalid_solution(self):
+        create_puzzle_range()
+        response = self.client.get(reverse('solution', args=[100]))
+        self.assertEqual(response.status_code, 404)
+
+    def test_invalid_preview(self):
+        create_puzzle_range()
+        response = self.client.get(reverse('preview', args=[100]))
+        self.assertEqual(response.status_code, 404)
+
+    def test_invalid_preview_solution(self):
+        create_puzzle_range()
+        response = self.client.get(reverse('preview_solution', args=[100]))
+        self.assertEqual(response.status_code, 404)
+
+    def test_index_list(self):
+        create_puzzle_range()
+        response = self.client.get(reverse('index'))
+        self.assertNotContains(response, reverse('puzzle', args=[4]))
+        self.assertNotContains(response, reverse('puzzle', args=[3]))
+        self.assertContains(response, reverse('puzzle', args=[2]))
+        self.assertContains(response, reverse('puzzle', args=[1]))
+        self.assertContains(response, reverse('puzzle', args=[0]))
+
+    def test_empty_index_list(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
