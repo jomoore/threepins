b22ea3608a31da3a055b801b7706ecada54b44ab
Jeremy Moore <jmoore@ieee.org>
2016-10-05 16:40:31 +0300

    Restored import order - loading settings really does need to be the first thing.

--- three_pins/wsgi.py
+++ three_pins/wsgi.py
@@ -6,10 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
+#pylint: disable=invalid-name,wrong-import-position
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "three_pins.settings")
+
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "three_pins.settings")
-application = DjangoWhiteNoise(get_wsgi_application()) #pylint: disable=invalid-name
+application = DjangoWhiteNoise(get_wsgi_application())
